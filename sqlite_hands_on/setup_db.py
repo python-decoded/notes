0cac8ff5f8036225a660b4de2be9d8e7c722f93a
python_decoded <python_decoded@gmail.com>
2025-04-24 21:05:02 +0300

    add sqlite

---
+++ sqlite_hands_on/setup_db.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+
+with sqlite3.connect("my_database.db") as connection:
+
+    cursor = connection.cursor()
+
+    cursor.executescript("""
+    CREATE TABLE IF NOT EXISTS Users (
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    """)
+
+    cursor.executescript("""
+    INSERT INTO Users (username, email) VALUES ('newuser', 'newuser@example.com');
+    INSERT INTO Users (username, email) VALUES ('other', 'other@example.com');
+    INSERT INTO Users (username, email) VALUES ('some', 'some@example.com');
+    """)
+
+    connection.commit()  # If there is no open transaction, this method is a no-op.
+
+    cursor.execute("SELECT * FROM Users")
+    rows = cursor.fetchall()
+    print(*rows, sep="\n")
+
+    row = cursor.execute("SELECT * FROM Users WHERE username = ?", ("other", )).fetchmany()
+    print("\n", row)
