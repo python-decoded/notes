77538120cd2b7cda1f9f4c15921d3ba1ddd7edcd
python_decoded <python_decoded@gmail.com>
2025-10-18 21:45:30 +0300

    fastapi 3 intermed state

--- fast_api_example/part_3/main.py
+++ fast_api_example/part_3/main.py
@@ -1,6 +1,7 @@
-import json
-from starlette.datastructures import UploadFile
-from fastapi import FastAPI, Request, Path, Query
+import json, uuid
+from typing import Annotated
+from starlette.datastructures import UploadFile as StarletteUploadFile
+from fastapi import FastAPI, Request, Path, Query, Header, Cookie, Body, Form, File, UploadFile,
 from fastapi.responses import JSONResponse
 from routers import books
 from core.handlers import add_handlers
@@ -10,29 +11,52 @@ app = FastAPI()
 add_handlers(app)
 
 
-@app.get("/echo/{something}")
-async def echo(req: Request,
-               something: str = Path(..., max_length=4),
-               foo: int = Query(1, lt=100)):
 
-    url = req.url
-    path_params = {"something": something}
-    query_params = {"foo": foo}
-    method = req.method
-    headers = req.headers
-    body = await req.form()
-    file = body["image"]
-    body = {k: v for k, v in body.items()
-            if not isinstance(v, UploadFile)}
 
-    return {"url": url, "path_params": path_params,
-            "query_params": query_params, "method": method,
-            "headers": headers, "body": body, "file": file.filename}
+
+@app.get("/echo")
+async def echo():
+    return f"Message: {data}"
 
 
 
 
 
+
+
+
+
+
+
+
+
+# @app.get("/echo/{something}")
+# async def echo(req: Request,
+#                req_body: Annotated[str, Body(None)],
+#                param1: Annotated[str, Form(...)],
+#                param2: Annotated[str, Form(None)],
+#                image: Annotated[UploadFile, File(None)],
+#                content_type: Annotated[str, Header("content-type")],
+#                session_id: Annotated[str, Cookie(None)],
+#                something: Annotated[str, Path(..., max_length=4)],
+#                foo: Annotated[int, Query(1, lt=100)]):
+#
+#     url = req.url
+#     path_params = {"something": something}
+#     query_params = {"foo": foo}
+#     # method = req.method
+#     # headers = req.headers
+#     # body = await req.form()
+#     # file = body["image"]
+#     # body = {k: v for k, v in body.items()
+#     #         if not isinstance(v, UploadFile)}
+#     #
+#     # return {"url": url, "path_params": path_params,
+#     #         "query_params": query_params, "method": method,
+#     #         "headers": headers, "body": body, "file": file.filename}
+#     #
+#
+#     return {"session_id": session_id, "contnet_type": content_type, "image": image}
 
 
 
