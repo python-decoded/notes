8fc4d10e03de893808882758a90a84d27de25851
python_decoded <python_decoded@gmail.com>
2025-10-19 21:31:52 +0300

    fastapi 3 final  state

--- fast_api_example/part_3/main.py
+++ fast_api_example/part_3/main.py
@@ -56,24 +56,24 @@ async def echo(req: Request,
             "user": user, "auth": authorization}
 
 
-# @app.middleware("http")
-# async def response_formatter(req: Request, call_next):
-#     # Щось зробити перед обробкою запиту
-#
-#     response = await call_next(req)
-#
-#     # Щось зробити після обробки запиту
-#     if response.headers.get("content-type") != "application/json":
-#         return response
-#
-#     raw_body = b"".join([chunk async for chunk in response.body_iterator])
-#     content = json.loads(raw_body.decode())
-#     if "detail" not in content:
-#         content = {"detail": content}
-#     content["middleware_was_here"] = True
-#
-#     return JSONResponse(status_code=response.status_code,
-#                         content=content)
+@app.middleware("http")
+async def response_formatter(req: Request, call_next):
+    # Щось зробити перед обробкою запиту
+
+    response = await call_next(req)
+
+    # Щось зробити після обробки запиту
+    if response.headers.get("content-type") != "application/json":
+        return response
+
+    raw_body = b"".join([chunk async for chunk in response.body_iterator])
+    content = json.loads(raw_body.decode())
+    if "detail" not in content:
+        content = {"detail": content}
+    content["middleware_was_here"] = True
+
+    return JSONResponse(status_code=response.status_code,
+                        content=content)
 
 
 @app.get("/status")
