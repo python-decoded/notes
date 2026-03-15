56ee6465bdb0b130546c5b387cd403e34858f1a5
python_decoded <python_decoded@gmail.com>
2026-03-15 09:39:56 +0200

    sql model examples

---
+++ notes_2026/sql_model/fast_api_sql_model_final.py
@@ -0,0 +1,62 @@
+from contextlib import asynccontextmanager
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine
+
+
+class User(SQLModel, table=True):
+
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    age: int | None = Field(default=None, index=True)
+    secret: str
+
+
+connect_args = {"check_same_thread": False}
+engine = create_engine("sqlite:///database.db", connect_args=connect_args)
+
+
+@asynccontextmanager
+async def lifespan(_):
+    SQLModel.metadata.create_all(bind=engine)
+    yield
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+app = FastAPI(lifespan=lifespan)
+
+
+
+
+
+
+@app.get("/users")
+def read_users(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[User]:
+    users = session.query(User).offset(offset).limit(limit).all()
+    return users
+
+
+@app.post("/users")
+def create_user(user: User, session: SessionDep) -> User:
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int, session: SessionDep) -> User:
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
