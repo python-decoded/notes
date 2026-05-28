c00bc4bce695734c95c9e60f43257f08e53c60c7
python_decoded <python_decoded@gmail.com>
2026-05-28 21:33:03 +0300

    annotation example

--- notes_2026/pydantic_validation/anotated_type_example.py
+++ notes_2026/pydantic_validation/anotated_type_example.py
@@ -1,12 +1,24 @@
-from pydantic import BaseModel, Field
+from datetime import datetime
+from pydantic import BaseModel, Field, AfterValidator
 
 import annotated_types
 from typing import Annotated
 PositiveInt = Annotated[int, Field(gt=0, le=100, multiple_of=5)]
 
 
+def check_18_plus(v: datetime) -> datetime:
+    today = datetime.today()
+    # чи був уже день народження цього року
+    age = today.year - v.year - ((today.month, today.day) < (v.month, v.day))
+
+    if age < 18:
+        raise ValueError("Вам має бути не менше 18 років")
+    return v
+
+
 class MyClass(BaseModel):
     value: PositiveInt
+    birth_date: Annotated[datetime, AfterValidator(check_18_plus)]
 
 
-MyClass(value=17)
+MyClass(value=10, birth_date="2016-04-25")
