4def6269d2b0f5266c3594e01998ee4c119aac62
python_decoded <python_decoded@gmail.com>
2026-07-12 10:01:19 +0300

    simple game

---
+++ torto/turtle_final.py
@@ -0,0 +1,379 @@
+import time
+import turtle
+import random
+from collections import defaultdict
+from functools import partial
+
+
+class UI:
+
+    font = ("Arial", 14, "bold")
+
+    def __init__(self, game):
+        self.game = game
+
+        self.t = turtle.Turtle()
+        self.t.hideturtle()
+        self.t.penup()
+        self.t.color("white")
+
+    def update(self):
+        self.t.clear()
+        self.t.goto(-380, 260)
+        self.t.write(f"HP: {self.game.player.hp}        KILLED: {self.game.kills}", font=self.font)
+
+
+class Controller:
+
+    def __init__(self, game):
+        self.game = game
+
+        self.pressed_keys = defaultdict(lambda: False)
+        self.mouse_down = False
+
+    def is_pressed(self, k) -> bool:
+        return self.pressed_keys[k]
+
+    def key_press(self, k):
+        self.pressed_keys[k] = True
+
+    def key_release(self, k):
+        self.pressed_keys[k] = False
+
+    def mouse_press(self, x, y):
+        self.mouse_down = True
+
+    def mouse_release(self, event):
+        self.mouse_down = False
+
+
+
+class Turtle(turtle.Turtle):
+
+    def __init__(self, game, *args, **kwargs):
+        self.game = game
+        super().__init__(*args, **kwargs)
+
+        self.penup()
+        self.speed(0)
+
+        self.setup()
+
+    def setup(self):
+        ...
+
+    def update(self):
+        ...
+
+    def process_collision(self, other: "Turtle"):
+        ...
+
+
+class Player(Turtle):
+
+    wait_fire_release = False
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.shape("triangle")
+        self.color("cyan")
+
+    def alive(self):
+        return self.hp > 0
+
+    def setup(self):
+        self.hp = 100
+        self.kills = 0
+        self.move_speed = 4
+        self.ammo = 5
+
+        self.home()
+
+    def rotate_towards_mouse_position(self, event):
+        if self.game.game_over:
+            return
+
+        x = event.x - self.game.screen.window_width() // 2
+        y = self.game.screen.window_height() // 2 - event.y
+        self.setheading(self.towards(x, y))
+
+    def move(self):
+        if self.game.controller.is_pressed("w"):
+            self.sety(self.ycor() + self.move_speed)
+        if self.game.controller.is_pressed("s"):
+            self.sety(self.ycor() - self.move_speed)
+        if self.game.controller.is_pressed("a"):
+            self.setx(self.xcor() - self.move_speed)
+        if self.game.controller.is_pressed("d"):
+            self.setx(self.xcor() + self.move_speed)
+
+    def fire_bullet(self):
+        if self.wait_fire_release:
+            if not self.game.controller.mouse_down:
+                self.wait_fire_release = False
+            return
+
+        if not self.game.controller.mouse_down or self.game.game_over:
+            return
+
+        if len(self.game.bullets) > self.ammo:
+            return
+
+        b = Bullet(self.game)
+        self.game.bullets.append(b)
+        self.wait_fire_release = True
+
+    def process_collision(self, other):
+
+        if isinstance(other, Enemy):
+            self.hp -= 1
+        elif isinstance(other, SpeedPack):
+            self.move_speed += 0.2
+            other.spawn()
+        elif isinstance(other, AmmoPack):
+            self.ammo += 1
+            other.spawn()
+
+    def update(self):
+        self.move()
+        self.fire_bullet()
+
+
+class Enemy(Turtle):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.shape("circle")
+        self.color("red")
+        self.spawn()
+
+    def spawn(self):
+        self.goto(random.randint(-380, 380), random.randint(-280, 280))
+
+    def update(self):
+        if self.game.game_over:
+            return
+
+        self.setheading(self.towards(self.game.player))
+        self.forward(self.game.enemy_speed)
+
+        if self.distance(self.game.player) < 20:
+            self.game.player.hp -= 1
+
+        if self.game.player.hp <= 0:
+            self.game.game_over = True
+
+    def process_collision(self, other: "Turtle"):
+        if isinstance(other, Bullet):
+            self.spawn()
+            self.game.kills += 1
+
+
+class SpeedPack(Turtle):
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.shape("square")
+        self.color("green")
+        self.spawn()
+
+    def spawn(self):
+        self.goto(random.randint(-380, 380), random.randint(-280, 280))
+
+    def update(self):
+        self.left(4)
+
+
+class AmmoPack(Turtle):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.shape("square")
+        self.color("yellow")
+        self.spawn()
+
+    def spawn(self):
+        self.goto(random.randint(-380, 380), random.randint(-280, 280))
+
+    def update(self):
+        self.left(4)
+
+
+class Bullet(Turtle):
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.shape("circle")
+        self.color("yellow")
+        self.shapesize(0.3)
+
+        self.goto(self.game.player.xcor(), self.game.player.ycor())
+        self.setheading(self.game.player.heading())
+
+        self.pendown()
+
+    def update(self):
+        if self.game.game_over:
+            return
+
+        self.forward(12)
+
+        if abs(self.xcor()) > 450 or abs(self.ycor()) > 350:
+            self.hideturtle()
+            self.game.bullets.remove(self)
+            return
+
+    def process_collision(self, other: "Turtle"):
+        if isinstance(other, Enemy):
+            self.hideturtle()
+
+            if self in self.game.bullets:
+                self.game.bullets.remove(self)
+
+
+class GameOverScreen(Turtle):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.color("white")
+        self.hideturtle()
+
+    def update(self):
+
+        if not self.game.game_over:
+            return
+
+        self.clear()
+        self.goto(0, 40)
+        self.write("💀 GAME OVER 💀", align="center", font=("Arial", 34, "bold"))
+
+        self.goto(0, -10)
+        self.write(f"Вбито ворогів: {self.game.kills}", align="center", font=("Arial", 24, "normal"))
+
+        self.goto(0, -60)
+        self.write("Натисни R щоб перезапустити", align="center", font=("Arial", 18, "normal"))
+
+
+# -----------------------------
+# Глобальні структури
+# -----------------------------
+
+class Game:
+
+    kills: int
+    enemy_speed: float
+    game_over: bool
+
+    def __init__(self):
+
+        self.enemies = []
+        self.bullets = []
+
+        self.screen = self.setup_screen()
+
+        self.hud = UI(self)
+        self.game_over_screen = GameOverScreen(self)
+
+        self.player = Player(self)
+        self.controller = Controller(self)
+        self.setup_listeners()
+
+        self.speed_pack = SpeedPack(self)
+        self.ammo_pack = AmmoPack(self)
+
+        self.setup_game()
+
+    def setup_screen(self):
+        screen = turtle.Screen()
+
+        screen.setup(800, 600)
+        screen.title("Top-Down Shooter (Turtle)")
+        screen.bgcolor("black")
+        screen.tracer(0)  # turtle animation off
+        return screen
+
+    def fill_bg(self):
+
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.fillcolor("black")
+
+        # 2. Start the filling process
+        t.setposition(-400, -300)
+        t.begin_fill()
+
+        # 3. Draw your shape (example: a square)
+        for _ in range(4):
+            t.forward(800)
+            t.left(800)
+
+        # 4. End the filling process
+        t.end_fill()
+
+    def setup_listeners(self):
+
+        self.screen.listen()
+
+        for k in "wasd":
+            self.screen.onkeypress(partial(self.controller.key_press, k), k)
+            self.screen.onkeyrelease(partial(self.controller.key_release, k), k)
+
+        self.screen.onkeypress(self.setup_game, "r")
+        self.screen.getcanvas().bind("<Motion>", self.player.rotate_towards_mouse_position)
+        self.screen.onscreenclick(self.controller.mouse_press, btn=1)
+        self.screen.getcanvas().bind("<ButtonRelease-1>", self.controller.mouse_release)
+
+    def setup_game(self):
+        self.fill_bg()
+
+        self.kills = 0
+        self.enemy_speed = 1.4
+
+        for b in self.bullets:
+            b.hideturtle()
+        for e in self.enemies:
+            e.hideturtle()
+
+        self.bullets = []
+        self.enemies = [Enemy(self) for _ in range(5)]
+
+        self.game_over = False
+        self.game_over_screen.clear()
+        self.hud.update()
+        self.player.setup()
+
+    def update(self):
+
+        if not self.game_over:
+            self.speed_pack.update()
+            self.ammo_pack.update()
+            self.player.update()
+
+            for b in self.bullets[:]:
+                b.update()
+
+            for e in self.enemies[:]:
+                e.update()
+
+            self.enemy_speed += 0.01  # over time enemies speed raises
+            self.precess_colision([self.player], [self.speed_pack, self.ammo_pack] + self.enemies)
+            self.precess_colision(self.bullets, self.enemies)
+
+        self.hud.update()
+        self.game_over_screen.update()
+        self.screen.update()
+
+    def precess_colision(self, collection1: list[Turtle], collection2: list[Turtle]):
+        for i in collection1[:]:
+            for j in sorted(collection2[:], key=lambda i: i.distance(i.position())):
+                if i.distance(*j.position()) < 20:
+                    i.process_collision(j)
+                    j.process_collision(i)
+
+
+game = Game()
+while True:
+    time.sleep(0.05)  # ~20 FPS
+    game.update()
