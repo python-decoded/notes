6ede273d5d9bf6de0fb7ff868c96f84223a504c5
python_decoded <python_decoded@gmail.com>
2026-07-12 23:28:54 +0300

    simple game

--- torto/turtle_game.py
+++ torto/turtle_game.py
@@ -2,6 +2,18 @@ import turtle
 import time
 
 
+class Player(turtle.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("triangle")
+        self.color("cyan")
+        self.hp = 100
+        self.move_speed = 4
+
+    def update(self):
+        ...
+
+
 class Game:
     def __init__(self):
         self.screen = turtle.Screen()
@@ -10,7 +22,11 @@ class Game:
         self.screen.bgcolor("black")
         self.screen.tracer(0)  # turtle animation off
 
+        self.player = Player()
+
     def update(self):
+        if self.player.hp > 0:
+            self.player.update()
 
         self.screen.update()
 
