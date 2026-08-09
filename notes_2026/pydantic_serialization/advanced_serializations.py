4783b686e31f6cdc66eb583832a1d4a76a25bc00
python_decoded <python_decoded>
2026-08-09 19:35:54 +0300

    pydantic validation examples

--- notes_2026/pydantic_serialization/advanced_serializations.py
+++ notes_2026/pydantic_serialization/advanced_serializations.py
@@ -14,5 +14,5 @@ class Order(BaseModel):
 
 order = Order(price=14.23, quantity=2,date="2025-10-15")
 
-print(order.model_dump())
+print(*order.model_dump().items(), sep='\n', end='\n\n')
 print(order.model_dump_json())
