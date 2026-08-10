1989ee7dbcd73b522112876ffad6015bc3b1b008
python_decoded <python_decoded>
2026-08-10 18:06:51 +0300

    pydantic validation examples

--- notes_2026/pydantic_serialization/serializing_model.py
+++ notes_2026/pydantic_serialization/serializing_model.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from pydantic import (BaseModel, Field, SecretStr, model_serializer,
-                      SerializerFunctionWrapHandler)
+                      SerializerFunctionWrapHandler, SerializationInfo)
 
 
 class Address(BaseModel):
@@ -14,11 +14,23 @@ class Address(BaseModel):
         data = serializer(self)
         return " ".join(str(v) for v in data.values())
 
+
 class User(BaseModel):
     name: str = Field()
     password: SecretStr = Field(exclude=True)
     birth_day: datetime
     address: Address
+
+    @model_serializer
+    def serialize(self, info: SerializationInfo):
+        data = {"name": self.name, "birth_day": self.birth_day, "address": self.address,
+                "password": self.password.get_secret_value()}
+
+        show_password = info.context and info.context.get("show_password")
+        if not show_password:
+            data.pop("password")
+
+        return data
 
 
 user = User(
@@ -28,5 +40,5 @@ user = User(
     address=Address(country="Ukraine", city="Kyiv", street="Protasova", house_number=15)
 )
 
-print(*user.model_dump().items(), sep='\n', end='\n\n')
-print(*user.model_dump_json().split(","), sep='\n', end='\n\n')
+print(*user.model_dump(context={"show_password": True}).items(), sep='\n', end='\n\n')
+print(*user.model_dump_json(context={"show_password": True}).split(","), sep='\n', end='\n\n')
