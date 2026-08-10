3a78fc771f9b7a34e9686f2ce729ac736a504ae7
python_decoded <python_decoded>
2026-08-10 09:33:55 +0300

    pydantic validation examples

--- notes_2026/pydantic_serialization/decorator_field_serializations.py
+++ notes_2026/pydantic_serialization/decorator_field_serializations.py
@@ -1,22 +1,24 @@
-from typing import Annotated
 from datetime import datetime
-from pydantic import BaseModel, computed_field, PlainSerializer
-
-
-def handler(v: float) -> str:
-    return format(v, ",.2f") + "$"
-
-DollarField = Annotated[float, PlainSerializer(handler, when_used="json")]
+from pydantic import (BaseModel, computed_field, field_serializer,
+                      SerializerFunctionWrapHandler)
 
 
 class Order(BaseModel):
-    price: DollarField
+    price: float
     quantity: int
     date: datetime
 
-    @computed_field(return_type=DollarField)
+    @computed_field
     def total(self) -> float:
         return self.price * self.quantity
+
+    @field_serializer("price", "total",
+                      when_used="json", mode="wrap")
+    def handler(self, v: float, serializer: SerializerFunctionWrapHandler) -> str:
+        # зробити щось до вбудованої серіалізації
+        v = serializer(v)
+        # зробити щось після вбудованої серіалізації
+        return format(v, ",.2f") + "$"
 
 
 order = Order(price=14.23, quantity=2,date="2025-10-15")
